0bc44a1a104b8a15968b3430b174db1ff942b9fe
mchandrapratama <144599226+mchandrapratama@users.noreply.github.com>
2023-09-10 19:08:52 +0700

    Add files via upload

---
+++ dart_application_1_chandra.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_1_chandra/dart_application_1_chandra.dart' as dart_application_1_chandra;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_1_chandra.calculate()}!');
+}
